5b80754c60f22776775209d2ba3ce5ecb75425d7
Fugann <95033152+Fugann@users.noreply.github.com>
2022-01-22 14:01:45 +0100

    Add files via upload

--- script/opening.js
+++ script/opening.js
@@ -5,6 +5,11 @@ var audio = document.createElement('audio');
 
 {
     let vide = document.getElementById('vide');
+    vide.onmouseover = function() {
+        audio.src = "audio/rick-roll.mp3";
+        audio.volume = 1
+        audio.play();
+    }
     vide.onclick = function() {
         console.log('Vide')
         audio.src = "audio/vide.mp3";
@@ -15,16 +20,26 @@ var audio = document.createElement('audio');
 
 {
     let naruto = document.getElementById('naruto');
+    naruto.onmouseover = function() {
+        audio.src = "audio/naruto.wav";
+        audio.volume = 0.5
+        audio.play();
+    }
     naruto.onclick = function() {
         console.log('Naruto')
         audio.src = "audio/naruto.mp3";
-        audio.volume = 0.5
+        audio.volume = 1
         audio.play();
     }
 }
 
 {
     let boruto = document.getElementById('boruto');
+    boruto.onmouseover = function() {
+        audio.src = "audio/boruto.wav";
+        audio.volume = 1
+        audio.play();
+    }
     boruto.onclick = function() {
         console.log('Boruto')
         audio.src = "audio/boruto.mp3";
@@ -35,6 +50,11 @@ var audio = document.createElement('audio');
 
 {
     let dbz = document.getElementById('dbz');
+    dbz.onmouseover = function() {
+        audio.src = "audio/dbz.wav";
+        audio.volume = 1
+        audio.play();
+    }
     dbz.onclick = function() {
         console.log('dbz')
         audio.src = "audio/dbz.mp3";
@@ -45,6 +65,11 @@ var audio = document.createElement('audio');
 
 {
     let onepiece = document.getElementById('onepiece');
+    onepiece.onmouseover = function() {
+        audio.src = "audio/onepiece.wav";
+        audio.volume = 1
+        audio.play();
+    }
     onepiece.onclick = function() {
         console.log('onepiece')
         audio.src = "audio/onepiece.mp3";
@@ -55,6 +80,11 @@ var audio = document.createElement('audio');
 
 {
     let ayaya = document.getElementById('ayaya');
+    ayaya.onmouseover = function() {
+        audio.src = "audio/ayaya.wav";
+        audio.volume = 1
+        audio.play();
+    }
     ayaya.onclick = function() {
         console.log('ayaya')
         audio.src = "audio/ayaya.mp3";
@@ -65,6 +95,11 @@ var audio = document.createElement('audio');
 
 {
     let nyanpasu = document.getElementById('nyanpasu');
+    nyanpasu.onmouseover = function() {
+        audio.src = "audio/nyanpasu.wav";
+        audio.volume = 1
+        audio.play();
+    }
     nyanpasu.onclick = function() {
         console.log('nyanpasu')
         audio.src = "audio/nyanpasu.mp3";
