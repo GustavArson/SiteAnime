4f51bd898c86b44357fd0dac66a836d16dc310cf
Fugann <95033152+Fugann@users.noreply.github.com>
2022-01-25 20:13:16 +0100

    Delete fighting-game.js

--- fighting-game.js
+++
@@ -1,7 +0,0 @@
-const face = document.querySelectorAll('.face')
-var numComments = face.length;
-for (var i = 0; i < numComments; i++) {
-    face[i].addEventListener('click', event => {
-        console.log(event.target.id);
-    }, false);
-}
